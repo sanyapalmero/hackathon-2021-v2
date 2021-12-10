82e21e54c075e9cbe79f8fe102283df3a006122a
Alexandr Vagner <sanyapalmero@gmail.com>
2021-12-10 18:42:59 +0500

    Add calling app

--- calling/urls.py
+++ calling/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "users"
+app_name = "calling"
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("users.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("calling.urls"))]
 
 if settings.DEBUG:
     urlpatterns = (
