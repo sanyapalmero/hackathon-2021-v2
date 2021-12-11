c0348b01e9c181648c517615ab0b49bff4d145ae
4madeuz <kosyanov1999@gmail.com>
2021-12-11 14:26:31 +0500

    add api for contacts

--- calling/views.py
+++ calling/views.py
@@ -1,5 +1,20 @@
 from django.views import generic
+from rest_framework import viewsets
+from rest_framework import filters
+
+from .serializers import ContactSerializer
+from .models import Contact
+
 
 
 class HomeView(generic.TemplateView):
     template_name = "calling/home.html"
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    filter_backends = (filters.SearchFilter,)
+    lookup_field = ('number')
+    search_fields = ('name', 'number') 
+    
