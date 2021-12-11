930495375a6d966b26f6e413ff0ca7f383b421f5
Vladimir556 <71672451+Vladimir556@users.noreply.github.com>
2021-12-12 00:31:33 +0500

    add RadioButton

--- static/ts/HackaphoneSpa.tsx
+++ static/ts/HackaphoneSpa.tsx
@@ -7,6 +7,7 @@ interface HackaphoneSpaState {
   activeCall: Call | null,
   isRegisted: boolean,
   isMuted: boolean,
+  isRadioEnabled: boolean,
 }
 
 export default class HackaphoneSpa extends Component<{}, HackaphoneSpaState> {
@@ -22,6 +23,7 @@ export default class HackaphoneSpa extends Component<{}, HackaphoneSpaState> {
       activeCall: null,
       isRegisted: false,
       isMuted: false,
+      isRadioEnabled: false,
     }
 
     this.client = CallingClient.get();
@@ -75,6 +77,24 @@ export default class HackaphoneSpa extends Component<{}, HackaphoneSpaState> {
     }
   }
 
+  async onPressRadioButton(event: h.JSX.TargetedMouseEvent<HTMLButtonElement>){
+    if (!this.state.isRadioEnabled) {
+      try {
+        this.setState({isRadioEnabled:true})
+        this.client.setOutMuted(true);
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    else {
+      try {
+        this.setState({isRadioEnabled:false})
+        this.client.setOutMuted(false);
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
 
   async acceptCall() {
     try {
@@ -177,20 +197,22 @@ export default class HackaphoneSpa extends Component<{}, HackaphoneSpaState> {
                 <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z" />
               </svg>
             </button>
-            <button className="btn-primary rounded-circle m-2 p-3 text-white shadow-sm border border-2 border-secondary" style="width: 10vh; height: 10vh;" data-bs-toggle="tooltip" data-bs-placement="top" title="Я - Радионяня">
+            <button onClick={(event) => {this.onPressRadioButton(event)}} className="btn-primary rounded-circle m-2 p-3 text-white shadow-sm border border-2 border-secondary position-relative" style="width: 10vh; height: 10vh;" data-bs-toggle="tooltip" data-bs-placement="top" title="Я - Радионяня">
+            <div className={this.state.isRadioEnabled ? "spinner-grow text-light position-absolute" : "spinner-grow text-light position-absolute d-none"} role="status" style="left:0;top:0;width:100%;height:100%;">
+            </div>
             <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-broadcast" viewBox="0 0 16 16">
             <path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
             </svg>
             </button>
             <button onClick={(event) => { this.changeVolume(event) }} className="btn-primary rounded-circle m-2 p-3 text-white shadow-sm border border-2 border-secondary" style="width: 10vh; height: 10vh;" data-bs-toggle="tooltip" data-bs-placement="top" title="Звук">
             {/* <!-- кнопка включенного звука --> */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-volume-up" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className={this.state.isMuted ? "bi bi-volume-up d-none" : "bi bi-volume-up"} viewBox="0 0 16 16">
               <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
               <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
               <path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/>
             </svg>
             {/* <!-- кнопка отключенного звука --> */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-volume-mute" viewBox="0 0 16 16" style="display: none;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className={this.state.isMuted ? "bi bi-volume-mute" : "bi bi-volume-mute d-none"} viewBox="0 0 16 16">
               <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zM6 5.04 4.312 6.39A.5.5 0 0 1 4 6.5H2v3h2a.5.5 0 0 1 .312.11L6 10.96V5.04zm7.854.606a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
             </svg>
             </button>
