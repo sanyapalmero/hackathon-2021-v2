507aa7517ef844a6715aa8f9d823025371369eb1
Oleg Yamnikov <oleg@oyam.dev>
2021-12-12 16:41:01 +0500

    Disable caching in the service worker

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,10 +1,7 @@
 // Base Service Worker implementation.  To use your own Service Worker, set the PWA_SERVICE_WORKER_PATH variable in settings.py
 
 var staticCacheName = "django-pwa-v" + new Date().getTime();
-var filesToCache = [
-  '/',
-  '/static/images/logo.png'
-];
+var filesToCache = [];
 
 // Cache on install
 self.addEventListener("install", event => {
